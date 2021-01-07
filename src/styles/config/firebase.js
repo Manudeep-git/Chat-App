cf3e1ac8fa47136009bd8db793c11038c65cc750
Manudeep-git <manudeepgadde01@gmail.com>
2021-01-07 20:47:14 +0530

    Add Firebase config and module

---
+++ src/styles/config/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+// since firebase is huge module, we only import /app from it
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCgCqcSyL7mzCwXiCsPw3xss2zQEnOKxYk",
+    authDomain: "chat-web-application-f6bdc.firebaseapp.com",
+    projectId: "chat-web-application-f6bdc",
+    storageBucket: "chat-web-application-f6bdc.appspot.com",
+    messagingSenderId: "466772619221",
+    appId: "1:466772619221:web:a734ff11b7bf557f2c975c",
+    measurementId: "G-C85KMQ3Q0T"
+};
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+export default app;
